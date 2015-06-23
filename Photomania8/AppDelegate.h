e6bc24c6da83b91fc8ba64057a57055f7571b3ef
BestKora <developer@bestkora.com>
2015-06-23 21:48:36 +0300

    Initial Commit

---
+++ Photomania8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photomania8
+//
+//  Created by Tatiana Kornilova on 6/23/15.
+//  Copyright (c) 2015 Tatiana Kornilova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
